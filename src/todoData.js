4c082adfd2f30e1ec88871a55524575c26bfdabd
borhanshuvo <borhan015@gmail.com>
2021-07-25 23:34:19 +0600

    changes

---
+++ src/todoData.js
@@ -0,0 +1,17 @@
+export const todoData = [
+  {
+    title: "I like it",
+    isComplete: false,
+    id: 1,
+  },
+  {
+    title: "I eat rice",
+    isComplete: true,
+    id: 2,
+  },
+  {
+    title: "I like travel",
+    isComplete: false,
+    id: 3,
+  },
+];
